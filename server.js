208b3a5d6d792d0252105a9a481401c3f9fbc109
Suchithz <suchithsomant@gmail.com>
2016-10-23 00:20:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content = {
+var articleOne = {
   title: 'Article One ! Suchith',
   heading: 'Article One',
   date: 'Oct 22',
