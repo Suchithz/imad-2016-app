64e1d14e5b2aef453f8e33cab77162086d3a6230
Suchithz <suchithsomant@gmail.com>
2016-10-31 14:38:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/:articleName', function (req, res) {
 });
 
 
-app.get('/main.js', function (req, res) {
+app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
