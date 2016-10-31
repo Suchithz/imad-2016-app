8e3f94dd7e4fdf5db2455f35d01cd1d3b1ad78d1
Suchithz <suchithsomant@gmail.com>
2016-10-31 14:33:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,11 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
+
+app.get('/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
